8b0cedc0df848791fac87efbf67109d58c33b618
rogerh3 <81320876+rogerh3@users.noreply.github.com>
2021-03-26 13:12:00 -0400

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Exception Examples
+		
+		//Example 1
+//		Scanner keyboard = new Scanner(System.in); 
+//		int num1 = keyboard.nextInt();
+//		int num2 = keyboard.nextInt(); 
+		
+//		int total = 0;
+//		total = num1/num2; 
+		
+//		System.out.println(total); 
+		//Arithmetic Exception in Main Thread if you divide by 0
+		
+		//Example 2
+//		int[] array = {1,5,20};
+		
+//		System.out.println(array[3]); 
+		//Array index mistake, the value of 20 would be the 2nd index
+		
+		
+		
+		
+	}
+
+}
